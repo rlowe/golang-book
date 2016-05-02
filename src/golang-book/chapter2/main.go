6f6f47ddc5c1195364587cf64582542763ffc00a
Ryan Lowe <rlowe@squareup.com>
2016-05-02 16:45:26 -0700

    Done with Chapter 3

--- src/golang-book/chapter2/main.go
+++ src/golang-book/chapter2/main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("Hello World, my name is Ryan!")
+  //fmt.Println("Hello World, my name is Ryan!")
+  fmt.Println(len("Hello world"))
+  fmt.Println("Hello world"[1])
+  fmt.Println("Hello " + "world")
 }

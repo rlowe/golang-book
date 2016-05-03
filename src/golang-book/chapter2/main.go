23ed37cd26cb464586a5ae8d09c0dd922d58981c
Ryan Lowe <rlowe@squareup.com>
2016-05-03 11:30:44 -0700

    Done with Chapter 4

--- src/golang-book/chapter2/main.go
+++ src/golang-book/chapter2/main.go
@@ -3,8 +3,14 @@ package main
 import "fmt"
 
 func main() {
-  //fmt.Println("Hello World, my name is Ryan!")
-  fmt.Println(len("Hello world"))
-  fmt.Println("Hello world"[1])
-  fmt.Println("Hello " + "world")
+  var x string
+  x = "first"
+  fmt.Println(x)
+  x = "second"
+  fmt.Println(x)
+  x = "third"
+  fmt.Println(x)
+  //fmt.Println(len("Hello world"))
+  //fmt.Println("Hello world"[1])
+  //fmt.Println("Hello " + "world")
 }
